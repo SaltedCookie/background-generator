2637c026e2e77e50c96dec3ae302d692cbabad1f
Kirill <boltin-1993@mail.ru>
2019-09-03 15:18:30 +0300

    Updates

---
+++ precache-manifest.be5c18de8263bb4af8620d440de511a4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a6b0637c0cc9da8b93d31bedf649fdc4",
+    "url": "/.github.io/Monsters-Cards-React/index.html"
+  },
+  {
+    "revision": "999e31ca3f77fd889b77",
+    "url": "/.github.io/Monsters-Cards-React/static/css/main.58a57c96.chunk.css"
+  },
+  {
+    "revision": "7b06fdf49ca34b60dc1c",
+    "url": "/.github.io/Monsters-Cards-React/static/js/2.7770e699.chunk.js"
+  },
+  {
+    "revision": "999e31ca3f77fd889b77",
+    "url": "/.github.io/Monsters-Cards-React/static/js/main.ee58f2b8.chunk.js"
+  },
+  {
+    "revision": "5725f28c025079d2787c",
+    "url": "/.github.io/Monsters-Cards-React/static/js/runtime~main.a57612e3.js"
+  }
+]);
